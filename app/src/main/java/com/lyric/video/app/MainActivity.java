12b1903cf8d4d0d1525490df2b623c90295feded
ganyu <ganyu@jihai8.com>
2018-09-14 13:50:19 +0800

    修改gradle版本为3.1.4

--- app/src/main/java/com/lyric/video/app/MainActivity.java
+++ app/src/main/java/com/lyric/video/app/MainActivity.java
@@ -3,37 +3,35 @@ package com.lyric.video.app;
 import android.app.Activity;
 import android.os.Bundle;
 
-import com.videoplayer.library.VideoPlayer;
+import com.videoplayer.library.ui.VideoMediaPlayerView;
 
 public class MainActivity extends Activity {
-    private static final String VIDEO_URL = "http://mvvideo2.meitudata.com/572e1dbe4fe681155.mp4";
-
-    private VideoPlayer mVideoPlayer;
+    private VideoMediaPlayerView mediaPlayerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mVideoPlayer = (VideoPlayer) findViewById(R.id.video_player);
+        mediaPlayerView = findViewById(R.id.video_media_player_view);
 
-        mVideoPlayer.play(VIDEO_URL);
+        mediaPlayerView.play("http://mvvideo2.meitudata.com/572e1dbe4fe681155.mp4");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        mVideoPlayer.onResume();
+        mediaPlayerView.onResume();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        mVideoPlayer.onPause();
+        mediaPlayerView.onPause();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        mVideoPlayer.onDestroy();
+        mediaPlayerView.onDestroy();
     }
 }
